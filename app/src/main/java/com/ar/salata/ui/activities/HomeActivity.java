17b24b9146537885498baef33543e5f8fbe93738
ahamdy96 <Hamdimn54@gmail.com>
2020-05-07 03:00:50 +0200

    fixed home fragment created twice on configurations change

--- app/src/main/java/com/ar/salata/ui/activities/HomeActivity.java
+++ app/src/main/java/com/ar/salata/ui/activities/HomeActivity.java
@@ -19,141 +19,142 @@ import com.ar.salata.ui.fragments.HomeFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    private DrawerLayout drawer;
-    private ActionBarDrawerToggle toggle;
-    private Toolbar toolbar;
-    private NavigationView navigationView;
-    private FragmentManager fragmentManager;
+	
+	private DrawerLayout drawer;
+	private ActionBarDrawerToggle toggle;
+	private Toolbar toolbar;
+	private NavigationView navigationView;
+	private FragmentManager fragmentManager;
 	private HomeFragment homeFragment;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
-
-        toolbar = findViewById(R.id.toolbar_home);
-        setSupportActionBar(toolbar);
-
-        setDrawer();
-
-        fragmentManager = getSupportFragmentManager();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_home);
+		getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+		
+		toolbar = findViewById(R.id.toolbar_home);
+		setSupportActionBar(toolbar);
+		
+		setDrawer();
+		
+		fragmentManager = getSupportFragmentManager();
 		homeFragment = HomeFragment.newInstance();
-
-        fragmentManager.beginTransaction()
+		
+		fragmentManager.beginTransaction()
 				.add(R.id.fragment_container_home, homeFragment)
-                .commit();
-
-    }
-
-    private void setDrawer() {
-        navigationView = findViewById(R.id.nav_view);
-
-        navigationView.setNavigationItemSelectedListener(this);
-
-        navigationView.getHeaderView(0).findViewById(R.id.btn_sign_in).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, SignInActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        drawer = findViewById(R.id.drawer);
-
-        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-
-        toggle.setDrawerIndicatorEnabled(true);
-        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorAccent));
-        toggle.syncState();
-
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        toggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Intent intent = null;
-        switch (item.getItemId()) {
-            case R.id.nav_main:
-                drawer.closeDrawer(GravityCompat.START);
-                break;
-            case R.id.nav_orders:
-                intent = new Intent(HomeActivity.this, OrdersActivity.class);
-                break;
-            case R.id.nav_settings:
-                // TODO: 3/30/2020
-                break;
-            case R.id.nav_address:
-                intent = new Intent(HomeActivity.this, AddAddressActivity.class);
-                break;
-            case R.id.nav_sign_up:
-                intent = new Intent(HomeActivity.this, SignUpActivity.class);
-                break;
-            case R.id.nav_about:
-                intent = new Intent(HomeActivity.this, AboutActivity.class);
-                break;
-            case R.id.nav_contact:
-                intent = new Intent(HomeActivity.this, ContactUsActivity.class);
-                break;
-            case R.id.nav_facebook:
-                // TODO: 3/30/2020
-                break;
-            case R.id.nav_twitter:
-                // TODO: 3/30/2020
-                break;
-            case R.id.nav_instagram:
-                // TODO: 3/30/2020
-                break;
-            case R.id.btn_sign_in:
-                intent = new Intent(HomeActivity.this, SignInActivity.class);
-                break;
-            case R.id.nav_sign_out:
-                drawer.closeDrawer(GravityCompat.START);
-                break;
-            default:
-                // TODO: 3/30/2020
-        }
-        if (intent != null) {
-            startActivity(intent);
-        }
-        navigationView.setCheckedItem(item);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (drawer != null) {
-            navigationView.setCheckedItem(R.id.nav_main);
-            drawer.closeDrawer(GravityCompat.START);
-        }
-    }
-
-    public void setEFABVisiblity(boolean isVisible) {
+				.commit();
+		
+	}
+	
+	private void setDrawer() {
+		navigationView = findViewById(R.id.nav_view);
+		
+		navigationView.setNavigationItemSelectedListener(this);
+		
+		navigationView.getHeaderView(0).findViewById(R.id.btn_sign_in).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HomeActivity.this, SignInActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		drawer = findViewById(R.id.drawer);
+		
+		toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		
+		toggle.setDrawerIndicatorEnabled(true);
+		toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorAccent));
+		toggle.syncState();
+		
+	}
+	
+	@Override
+	public void onConfigurationChanged(@NonNull Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		toggle.onConfigurationChanged(newConfig);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (toggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+		Intent intent = null;
+		switch (item.getItemId()) {
+			case R.id.nav_main:
+				drawer.closeDrawer(GravityCompat.START);
+				break;
+			case R.id.nav_orders:
+				intent = new Intent(HomeActivity.this, OrdersActivity.class);
+				break;
+			case R.id.nav_settings:
+				// TODO: 3/30/2020
+				break;
+			case R.id.nav_address:
+				intent = new Intent(HomeActivity.this, AddAddressActivity.class);
+				break;
+			case R.id.nav_sign_up:
+				intent = new Intent(HomeActivity.this, SignUpActivity.class);
+				break;
+			case R.id.nav_about:
+				intent = new Intent(HomeActivity.this, AboutActivity.class);
+				break;
+			case R.id.nav_contact:
+				intent = new Intent(HomeActivity.this, ContactUsActivity.class);
+				break;
+			case R.id.nav_facebook:
+				// TODO: 3/30/2020
+				break;
+			case R.id.nav_twitter:
+				// TODO: 3/30/2020
+				break;
+			case R.id.nav_instagram:
+				// TODO: 3/30/2020
+				break;
+			case R.id.btn_sign_in:
+				intent = new Intent(HomeActivity.this, SignInActivity.class);
+				break;
+			case R.id.nav_sign_out:
+				drawer.closeDrawer(GravityCompat.START);
+				break;
+			default:
+				// TODO: 3/30/2020
+		}
+		if (intent != null) {
+			startActivity(intent);
+		}
+		navigationView.setCheckedItem(item);
+		return true;
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (drawer != null) {
+			navigationView.setCheckedItem(R.id.nav_main);
+			drawer.closeDrawer(GravityCompat.START);
+		}
+	}
+	
+	public void setEFABVisiblity(boolean isVisible) {
 		homeFragment.setEFABVisiblity(isVisible);
-    }
+	}
 }
--- app/src/main/java/com/ar/salata/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/ar/salata/ui/fragments/HomeFragment.java
@@ -24,96 +24,98 @@ import com.google.android.material.navigation.NavigationView;
 import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment {
-    private static final int DIALOGREQUESTCODE = 1;
-    private ExtendedFloatingActionButton eFABWeigh;
-    private DrawerLayout drawer;
-    private ActionBarDrawerToggle toggle;
-    private Toolbar toolbar;
-    private NavigationView navigationView;
-    
-    
-    public static HomeFragment newInstance() {
-
-        Bundle args = new Bundle();
-        
-        HomeFragment fragment = new HomeFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container);
-
-
-        eFABWeigh = view.findViewById(R.id.efab_weigh);
-
-        eFABWeigh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: 4/27/2020 assert user is logged in
-                ChooseAddressDialogFragment dialogFragment = new ChooseAddressDialogFragment();
-                dialogFragment.show(getActivity().getSupportFragmentManager(), null);
-    
-                dialogFragment.setTargetFragment(HomeFragment.this, DIALOGREQUESTCODE);
-            }
-        });
-
-        toolbar = view.findViewById(R.id.toolbar_home);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-
-        setDrawer();
-
-
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    private void setDrawer() {
-        navigationView = getActivity().findViewById(R.id.nav_view);
-
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) getActivity());
-
-        drawer = getActivity().findViewById(R.id.drawer);
-        toggle = new ActionBarDrawerToggle(getActivity(), drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.setDrawerIndicatorEnabled(true);
-        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorAccent));
-        toggle.syncState();
-
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        toggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == DIALOGREQUESTCODE && resultCode == RESULT_OK) {
-            Intent intent = new Intent(getContext(), AddToCartActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    public void setEFABVisiblity(boolean isVisible) {
-        if (isVisible)
-            eFABWeigh.show();
-        else
-            eFABWeigh.hide();
-    }
+	private static final int DIALOGREQUESTCODE = 1;
+	private ExtendedFloatingActionButton eFABWeigh;
+	private DrawerLayout drawer;
+	private ActionBarDrawerToggle toggle;
+	private Toolbar toolbar;
+	private NavigationView navigationView;
+	
+	
+	public static HomeFragment newInstance() {
+		
+		Bundle args = new Bundle();
+		
+		HomeFragment fragment = new HomeFragment();
+		fragment.setArguments(args);
+		return fragment;
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		if (requestCode == DIALOGREQUESTCODE && resultCode == RESULT_OK) {
+			Intent intent = new Intent(getContext(), AddToCartActivity.class);
+			startActivity(intent);
+		}
+	}
+	
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		if (savedInstanceState != null) return null;
+		
+		View view = inflater.inflate(R.layout.fragment_home, container);
+		
+		
+		eFABWeigh = view.findViewById(R.id.efab_weigh);
+		
+		eFABWeigh.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO: 4/27/2020 assert user is logged in
+				ChooseAddressDialogFragment dialogFragment = new ChooseAddressDialogFragment();
+				dialogFragment.show(getActivity().getSupportFragmentManager(), null);
+				
+				dialogFragment.setTargetFragment(HomeFragment.this, DIALOGREQUESTCODE);
+			}
+		});
+		
+		toolbar = view.findViewById(R.id.toolbar_home);
+		((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+		
+		setDrawer();
+		
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+	}
+	
+	@Override
+	public void onConfigurationChanged(@NonNull Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		toggle.onConfigurationChanged(newConfig);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (toggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void setDrawer() {
+		navigationView = getActivity().findViewById(R.id.nav_view);
+		
+		navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) getActivity());
+		
+		drawer = getActivity().findViewById(R.id.drawer);
+		toggle = new ActionBarDrawerToggle(getActivity(), drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.setDrawerIndicatorEnabled(true);
+		toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorAccent));
+		toggle.syncState();
+		
+	}
+	
+	public void setEFABVisiblity(boolean isVisible) {
+		if (isVisible)
+			eFABWeigh.show();
+		else
+			eFABWeigh.hide();
+	}
 }
