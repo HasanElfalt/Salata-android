90b752d113c414cd3330c38bdb5b866ad8f3b2c7
Rokaya <rokaya.i.hussain@gmail.com>
2020-06-26 11:31:23 +0200

    update password activity , and user profile activity create

---
+++ app/src/main/java/com/ar/salata/ui/activities/UserProfileِActivity.java
@@ -0,0 +1,14 @@
+package com.ar.salata.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileِActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
